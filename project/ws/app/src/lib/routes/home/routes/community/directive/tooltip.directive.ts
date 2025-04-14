1f163b94cf3eea11d06bd5f59b52d27b91c955ad
bharathravi-in <bharathravi.in@gmail.com>
2025-04-14 11:13:00 +0530

    changes in community guidelines info and community name check

--- project/ws/app/src/lib/routes/home/routes/community/directive/tooltip.directive.ts
+++ project/ws/app/src/lib/routes/home/routes/community/directive/tooltip.directive.ts
@@ -1,87 +1,73 @@
-import { Directive, Input, ElementRef, HostListener, ComponentRef, ViewContainerRef, ComponentFactoryResolver } from '@angular/core'
+import {
+  Directive,
+  Input,
+  ElementRef,
+  ComponentRef,
+  HostListener,
+  ViewContainerRef,
+  OnDestroy,
+  NgZone
+} from '@angular/core'
 import { TooltipComponent } from './tooltip/tooltip.component'
 
 @Directive({
   selector: '[appTooltip]'
 })
-export class TooltipDirective {
-  @Input('appTooltip') tooltipContent: string = '';
-  @Input() tooltipPosition: 'top' | 'bottom' | 'left' | 'right' = 'left';
-  @Input() showError: boolean = false;
-  private tooltipComponentRef: ComponentRef<TooltipComponent> | null = null;
+export class TooltipDirective implements OnDestroy {
+  @Input('appTooltip') content: string = '';
+  @Input() position: 'top' | 'bottom' | 'left' | 'right' = 'top';
+
+  private tooltipRef: ComponentRef<TooltipComponent> | null = null;
+  private showTimeout: any
+  private hideTimeout: any
 
   constructor(
-    private elementRef: ElementRef,
-    private viewContainerRef: ViewContainerRef,
-    private componentFactoryResolver: ComponentFactoryResolver
+    private el: ElementRef,
+    private vcr: ViewContainerRef,
+    private ngZone: NgZone
   ) { }
 
   @HostListener('mouseenter')
   onMouseEnter(): void {
-    if (!this.tooltipComponentRef && this.showError) {
-      const factory = this.componentFactoryResolver.resolveComponentFactory(TooltipComponent)
-      this.tooltipComponentRef = this.viewContainerRef.createComponent(factory)
-      this.tooltipComponentRef.instance.tooltipContent = this.tooltipContent
-      this.setPosition()
-    }
+    this.ngZone.run(() => {
+      clearTimeout(this.hideTimeout)
+      this.showTimeout = setTimeout(() => {
+        if (this.tooltipRef === null) {
+          this.tooltipRef = this.vcr.createComponent(TooltipComponent)
+          this.tooltipRef.instance.content = this.content
+          this.tooltipRef.instance.position = this.position
+
+          // Use requestAnimationFrame to ensure the component is rendered before positioning
+          requestAnimationFrame(() => {
+            if (this.tooltipRef) {
+              this.tooltipRef.instance.setPosition(this.el)
+            }
+          })
+        }
+      }, 100) // Small delay to prevent accidental triggers
+    })
   }
 
   @HostListener('mouseleave')
   onMouseLeave(): void {
-    this.destroyTooltip()
-  }
-
-  private setPosition(): void {
-    if (this.tooltipComponentRef) {
-      const hostElem = this.elementRef.nativeElement
-      const tooltipElem = this.tooltipComponentRef.location.nativeElement
-      const hostRect = hostElem.getBoundingClientRect()
-      const tooltipRect = tooltipElem.getBoundingClientRect()
-
-      let top = 0
-      let left = 0
-
-      console.log('Host Rect:', hostRect)
-      console.log('Tooltip Rect:', tooltipRect)
-      console.log('Position:', this.tooltipPosition)
-
-      switch (this.tooltipPosition) {
-        case 'top':
-          top = hostRect.top - tooltipRect.height - 5
-          left = hostRect.left + (hostRect.width - tooltipRect.width) / 2
-          break
-        case 'bottom':
-          top = hostRect.bottom + 5
-          left = hostRect.left + (hostRect.width - tooltipRect.width) / 2
-          break
-        case 'left':
-          top = hostRect.top + (hostRect.height - tooltipRect.height) / 2
-          left = hostRect.left - tooltipRect.width - 5
-          break
-        case 'right':
-          top = hostRect.top + (hostRect.height - tooltipRect.height) / 2
-          left = hostRect.right + 5
-          break
-      }
-
-      // Add scroll position after calculating relative positions
-      top += window.scrollY
-      left += window.scrollX
-
-      console.log('Final Position:', { top, left })
-
-      tooltipElem.style.position = 'absolute'
-      tooltipElem.style.top = `${top}px`
-      tooltipElem.style.left = `${left}px`
-      tooltipElem.style.display = 'block'
-      tooltipElem.style.zIndex = '9999'
-    }
+    this.ngZone.run(() => {
+      clearTimeout(this.showTimeout)
+      this.hideTimeout = setTimeout(() => {
+        this.destroyTooltip()
+      }, 100) // Small delay before hiding
+    })
   }
 
   private destroyTooltip(): void {
-    if (this.tooltipComponentRef) {
-      this.tooltipComponentRef.destroy()
-      this.tooltipComponentRef = null
+    if (this.tooltipRef !== null) {
+      this.tooltipRef.destroy()
+      this.tooltipRef = null
     }
+  }
+
+  ngOnDestroy(): void {
+    clearTimeout(this.showTimeout)
+    clearTimeout(this.hideTimeout)
+    this.destroyTooltip()
   }
 }
