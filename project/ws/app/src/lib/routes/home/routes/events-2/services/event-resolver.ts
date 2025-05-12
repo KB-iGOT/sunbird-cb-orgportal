2c5e58f141505ac57c4e6515e6fda598556113ba
mansurskTarento <mansur.shaik@tarento.com>
2025-05-12 16:14:06 +0530

    solving event edit and rejected directly is not able to edit name and description

--- project/ws/app/src/lib/routes/home/routes/events-2/services/event-resolver.ts
+++ project/ws/app/src/lib/routes/home/routes/events-2/services/event-resolver.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core'
-import { catchError, map } from 'rxjs/operators'
+import { catchError, map, switchMap } from 'rxjs/operators'
 import { of, Observable } from 'rxjs'
 import { ActivatedRoute } from '@angular/router'
 import { EventsService } from './events.service'
@@ -17,8 +17,34 @@ export class EventResolverService {
     if (queryParams['mode'] === 'view' && queryParams['pathUrl'] === 'upcoming') {
       getLiveData = true
     }
-    return this.eventSvc.getEventDetailsByid(id, getLiveData)
-      .pipe(
+
+    if (queryParams['pathUrl'] === 'rejected' || queryParams['pathUrl'] === 'draft') {
+      const callWithMode = this.eventSvc.getEventDetailsByid(id, false)
+      const callWithOutMode = this.eventSvc.getEventDetailsByid(id, true)
+
+      return callWithMode.pipe(
+        map((responseFalse: any) => {
+          const requiredData = _.get(responseFalse, 'result.event')
+          return callWithOutMode.pipe(
+            map((responseTrue: any) => {
+              const liveStatus = _.get(responseTrue, 'result.event.status')
+              if (liveStatus.toLowerCase() !== 'live') {
+                delete requiredData['prevStatus']
+              }
+              return { data: requiredData, error: null }
+            }),
+            catchError((err: any) => {
+              return of({ data: null, error: err })
+            })
+          )
+        }),
+        catchError((err: any) => {
+          return of({ data: null, error: err })
+        }),
+        switchMap((result: any) => result)
+      )
+    } else {
+      return this.eventSvc.getEventDetailsByid(id, getLiveData).pipe(
         map((data: any) => {
           const requiredData = _.get(data, 'result.event')
           return { data: requiredData, error: null }
@@ -27,5 +53,6 @@ export class EventResolverService {
           return of({ data: null, error: err })
         })
       )
+    }
   }
 }
