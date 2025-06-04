ea1a7b50de92d3d818cbd50b3dad4facb0ea91b5
Venkat Kandagaddala <venky.kandagaddala@gmail.com>
2025-06-04 14:43:00 +0530

    Trigger telemetry event when user clicks on notification

--- project/ws/app/src/lib/routes/home/routes/my-notifications/my-notifications.component.ts
+++ project/ws/app/src/lib/routes/home/routes/my-notifications/my-notifications.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core'
 import { Router } from '@angular/router'
+import { EventService } from '@sunbird-cb/utils'
 
 @Component({
   selector: 'ws-app-my-notifications',
@@ -8,10 +9,11 @@ import { Router } from '@angular/router'
 })
 export class MyNotificationsComponent {
 
-  constructor(private router: Router,) { }
+  constructor(private router: Router, private events: EventService) { }
 
   redirectTo(notification: any) {
     if (notification.category) {
+      this.raiseTelemetryEventForNotification(notification)
       if (notification.category === 'PROFILE') {
         this.router.navigate([`app/home/approvals/approval`])
       } else {
@@ -20,6 +22,20 @@ export class MyNotificationsComponent {
     } else {
       this.router.navigate(['/app/home/notifications'], { queryParams: { tab: notification } })
     }
+  }
+
+  raiseTelemetryEventForNotification(notification: any) {
+    this.events.raiseInteractTelemetry(
+      {
+        type: 'click',
+        subType: 'notification-engine',
+        id: notification.notification_id,
+      },
+      {},
+      {
+        module: 'Home',
+      }
+    )
   }
 
 }
