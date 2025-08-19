603d0eeeed018ef406153985b456198083c71bf0
bharathravi-in <bharathravi.in@gmail.com>
2025-08-19 11:20:17 +0530

    changes  inthe mdo-channel-v3

---
+++ project/ws/app/src/lib/routes/home/routes/microsite/microsite.module.ts
@@ -0,0 +1,33 @@
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+
+import {
+  SlidersLibModule,
+  MDOChannelModule,
+} from '@sunbird-cb/consumption'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { MicrositeV1Component } from './microsite-v1/microsite-v1.component'
+import { MicrositeRoutingModule } from './microsite-routing.module'
+import { TranslateModule } from '@ngx-translate/core'
+import { MatLegacyDialogModule } from '@angular/material/legacy-dialog'
+
+// Removed getTranslateModule function as it's no longer needed
+
+@NgModule({
+  declarations: [MicrositeV1Component],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MicrositeRoutingModule,
+    SlidersLibModule,
+    MDOChannelModule,
+    TranslateModule.forChild(),
+    MatLegacyDialogModule
+  ],
+  providers: [
+  ],
+
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+export class MicrositeModule { }
