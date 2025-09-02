0678bac6e3c13a290ce212a8b4a5c5341b0fc73d
vishnubansaltarento <vishnu.bansal@tarento.com>
2025-09-02 11:26:07 +0530

    update for course category changes for search

--- project/ws/app/src/lib/routes/training-plan/components/search/search.component.ts
+++ project/ws/app/src/lib/routes/training-plan/components/search/search.component.ts
@@ -142,8 +142,8 @@ export class SearchComponent implements OnInit {
         request: {
           secureSettings: contentType === 'Moderated Course' ? true : false, // for moderated course
           filters: {
-            primaryCategory: [contentType === 'Moderated Course' ? 'Course' : contentType],
-            // courseCategory: [contentType === 'Moderated Course' ? 'Course' : contentType],
+            //primaryCategory: [contentType === 'Moderated Course' ? 'Course' : contentType],
+            courseCategory: [contentType === 'Moderated Course' ? 'Course' : contentType],
             /* tslint:disable */
             organisation: applyFilterObj && applyFilterObj['providers'] && applyFilterObj['providers'].length ? applyFilterObj['providers'] : [],
             [`${this.compentencyKey.vKey}.${this.compentencyKey.vCompetencyArea}`]: applyFilterObj && applyFilterObj[this.compentencyKey.vCompetencyArea] && applyFilterObj[this.compentencyKey.vCompetencyArea].length ? applyFilterObj[this.compentencyKey.vCompetencyArea] : [],
