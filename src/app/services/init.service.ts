7046d7713faab51de35886344d71a01743b85fca
Sohith-code <kanala.sohith@tarento.com>
2025-09-22 23:17:29 +0530

    Handled competency keys (#454)

--- src/app/services/init.service.ts
+++ src/app/services/init.service.ts
@@ -253,7 +253,7 @@ export class InitService {
     // TODO: set one org as default org :: use user preference
     this.configSvc.activeOrg = publicConfig.org[0]
     this.configSvc.appSetup = publicConfig.appSetup
-    this.configSvc.compentency = publicConfig.compentency
+    this.configSvc.compentency = publicConfig.compentency || publicConfig.competency
 
     return publicConfig
   }
