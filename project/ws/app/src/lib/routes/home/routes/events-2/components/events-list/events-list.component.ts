0d8730935a49989b8697b02e410235c61f0dfb7d
Sohith Kanala <kanala.sohith@tarento.com>
2025-10-13 17:09:56 +0530

    Updated packages search and consumption

--- project/ws/app/src/lib/routes/home/routes/events-2/components/events-list/events-list.component.ts
+++ project/ws/app/src/lib/routes/home/routes/events-2/components/events-list/events-list.component.ts
@@ -165,7 +165,7 @@ export class EventsListComponent implements OnInit, OnDestroy {
           },
         ]
         break
-      case 'canceled':
+      case 'cancelled':
         this.tableData = {
           columns: [
             { displayName: 'Event Name', key: 'name', cellType: 'textImage', imageKey: 'appIcon', cellClass: 'text-overflow-elipse' },
