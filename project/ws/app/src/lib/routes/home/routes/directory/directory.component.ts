d2a3588b00c4d8dfa92532273cca87bcb2c1257b
srikanthmutte <srikanth.mutte@tarento.com>
2026-07-10 14:56:34 +0530

    volunteer organisation tab updated

--- project/ws/app/src/lib/routes/home/routes/directory/directory.component.ts
+++ project/ws/app/src/lib/routes/home/routes/directory/directory.component.ts
@@ -25,7 +25,7 @@ export class DirectoryComponent implements OnInit, AfterViewInit {
   tabs: any = [
     { name: 'Organisation', value: 'organisation' },
     { name: 'Organisation Hierarchies', value: 'organisationHierarchies' },
-    { name: 'Volunteer Organisation', value: 'volunteer' }
+    { name: 'Volunteer Organisations', value: 'volunteer' }
   ]
 
   constructor(
